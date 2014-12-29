c7d5563f1283deca742b6ece4dab779f14fe8ec8
Vinny Brown <busidex.vinbrown@gmail.com>
2014-12-28 20:35:11 -0500

    update Newtonsoft.Json, fix android layouts

--- Busidex.Presentation.Android/Resources/Resource.designer.cs
+++ Busidex.Presentation.Android/Resources/Resource.designer.cs
@@ -50,6 +50,9 @@ namespace Busidex.Presentation.Android
 			// aapt resource value: 0x7f040001
 			public const int labelFontColor = 2130968577;
 			
+			// aapt resource value: 0x7f040002
+			public const int layoutBackground = 2130968578;
+			
 			static Color()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -64,10 +67,22 @@ namespace Busidex.Presentation.Android
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int delete = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int LargeLogo = 2130837505;
+			public const int icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon_2x = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int large_logo = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int people = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int spotlight_icon = 2130837509;
 			
 			static Drawable()
 			{
@@ -82,20 +97,50 @@ namespace Busidex.Presentation.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060002
-			public const int btnConnect = 2131099650;
+			// aapt resource value: 0x7f06000d
+			public const int btnConnect = 2131099661;
 			
-			// aapt resource value: 0x7f060003
-			public const int btnStart = 2131099651;
+			// aapt resource value: 0x7f060005
+			public const int btnLogin = 2131099653;
+			
+			// aapt resource value: 0x7f06000e
+			public const int btnStart = 2131099662;
+			
+			// aapt resource value: 0x7f06000a
+			public const int button1 = 2131099658;
+			
+			// aapt resource value: 0x7f06000b
+			public const int button2 = 2131099659;
+			
+			// aapt resource value: 0x7f06000c
+			public const int button3 = 2131099660;
+			
+			// aapt resource value: 0x7f060002
+			public const int editText1 = 2131099650;
 			
 			// aapt resource value: 0x7f060004
-			public const int imgLogo = 2131099652;
+			public const int editText2 = 2131099652;
+			
+			// aapt resource value: 0x7f060007
+			public const int imageView1 = 2131099655;
+			
+			// aapt resource value: 0x7f060008
+			public const int imageView2 = 2131099656;
+			
+			// aapt resource value: 0x7f060009
+			public const int imageView3 = 2131099657;
+			
+			// aapt resource value: 0x7f060006
+			public const int imgLogo = 2131099654;
 			
 			// aapt resource value: 0x7f060000
-			public const int textView1 = 2131099648;
+			public const int relativeLayout1 = 2131099648;
 			
 			// aapt resource value: 0x7f060001
-			public const int textView2 = 2131099649;
+			public const int textView1 = 2131099649;
+			
+			// aapt resource value: 0x7f060003
+			public const int textView2 = 2131099651;
 			
 			static Id()
 			{
@@ -111,7 +156,13 @@ namespace Busidex.Presentation.Android
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Login = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int StartUp = 2130903042;
 			
 			static Layout()
 			{
@@ -126,26 +177,41 @@ namespace Busidex.Presentation.Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int StartPage_btnConnect = 2131034116;
+			// aapt resource value: 0x7f05000b
+			public const int Login_btnLogin = 2131034123;
+			
+			// aapt resource value: 0x7f05000a
+			public const int Login_lblPassword = 2131034122;
+			
+			// aapt resource value: 0x7f050009
+			public const int Login_lblUserName = 2131034121;
+			
+			// aapt resource value: 0x7f050007
+			public const int Main_btnMyBusidex = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int Main_btnMyOrganizations = 2131034120;
 			
 			// aapt resource value: 0x7f050006
-			public const int StartPage_btnStart = 2131034118;
+			public const int Main_btnSearch = 2131034118;
 			
 			// aapt resource value: 0x7f050003
-			public const int StartPage_lblAlreadyAUser = 2131034115;
+			public const int StartPage_btnConnect = 2131034115;
 			
 			// aapt resource value: 0x7f050005
-			public const int StartPage_lblNewUser = 2131034117;
+			public const int StartPage_btnStart = 2131034117;
 			
 			// aapt resource value: 0x7f050002
-			public const int StartPage_lblWelcome = 2131034114;
+			public const int StartPage_lblAlreadyAUser = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int StartPage_lblNewUser = 2131034116;
 			
 			// aapt resource value: 0x7f050001
-			public const int app_name = 2131034113;
+			public const int StartPage_lblWelcome = 2131034113;
 			
 			// aapt resource value: 0x7f050000
-			public const int hello = 2131034112;
+			public const int app_name = 2131034112;
 			
 			static String()
 			{
