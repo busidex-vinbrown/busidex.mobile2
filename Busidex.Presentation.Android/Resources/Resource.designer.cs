c443648adb326e080e345eed5de6262e54925519
Vinny Brown <busidex.vinbrown@gmail.com>
2014-12-29 22:14:15 -0500

    Add card view. Fix divider line in MyBusidex list view

--- Busidex.Presentation.Android/Resources/Resource.designer.cs
+++ Busidex.Presentation.Android/Resources/Resource.designer.cs
@@ -100,65 +100,71 @@ namespace Busidex.Presentation.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f06000e
-			public const int btnConnect = 2131099662;
-			
-			// aapt resource value: 0x7f060005
-			public const int btnLogin = 2131099653;
-			
-			// aapt resource value: 0x7f06000b
-			public const int btnMyBusidex = 2131099659;
-			
-			// aapt resource value: 0x7f06000c
-			public const int btnMyOrganizations = 2131099660;
-			
-			// aapt resource value: 0x7f06000a
-			public const int btnSearch = 2131099658;
-			
 			// aapt resource value: 0x7f06000f
-			public const int btnStart = 2131099663;
+			public const int btnConnect = 2131099663;
 			
-			// aapt resource value: 0x7f060007
-			public const int imageView1 = 2131099655;
-			
-			// aapt resource value: 0x7f060008
-			public const int imageView2 = 2131099656;
-			
-			// aapt resource value: 0x7f060009
-			public const int imageView3 = 2131099657;
-			
-			// aapt resource value: 0x7f060012
-			public const int imgCardHorizontal = 2131099666;
-			
-			// aapt resource value: 0x7f060013
-			public const int imgCardVertical = 2131099667;
+			// aapt resource value: 0x7f060015
+			public const int btnInfo = 2131099669;
 			
 			// aapt resource value: 0x7f060006
-			public const int imgLogo = 2131099654;
+			public const int btnLogin = 2131099654;
+			
+			// aapt resource value: 0x7f06000c
+			public const int btnMyBusidex = 2131099660;
 			
 			// aapt resource value: 0x7f06000d
-			public const int lstCards = 2131099661;
+			public const int btnMyOrganizations = 2131099661;
 			
-			// aapt resource value: 0x7f060000
-			public const int relativeLayout1 = 2131099648;
-			
-			// aapt resource value: 0x7f060001
-			public const int textView1 = 2131099649;
-			
-			// aapt resource value: 0x7f060003
-			public const int textView2 = 2131099651;
-			
-			// aapt resource value: 0x7f060011
-			public const int txtCompanyName = 2131099665;
+			// aapt resource value: 0x7f06000b
+			public const int btnSearch = 2131099659;
 			
 			// aapt resource value: 0x7f060010
-			public const int txtName = 2131099664;
+			public const int btnStart = 2131099664;
 			
-			// aapt resource value: 0x7f060004
-			public const int txtPassword = 2131099652;
+			// aapt resource value: 0x7f060008
+			public const int imageView1 = 2131099656;
+			
+			// aapt resource value: 0x7f060009
+			public const int imageView2 = 2131099657;
+			
+			// aapt resource value: 0x7f06000a
+			public const int imageView3 = 2131099658;
+			
+			// aapt resource value: 0x7f060000
+			public const int imgCard = 2131099648;
+			
+			// aapt resource value: 0x7f060013
+			public const int imgCardHorizontal = 2131099667;
+			
+			// aapt resource value: 0x7f060014
+			public const int imgCardVertical = 2131099668;
+			
+			// aapt resource value: 0x7f060007
+			public const int imgLogo = 2131099655;
+			
+			// aapt resource value: 0x7f06000e
+			public const int lstCards = 2131099662;
+			
+			// aapt resource value: 0x7f060001
+			public const int relativeLayout1 = 2131099649;
 			
 			// aapt resource value: 0x7f060002
-			public const int txtUserName = 2131099650;
+			public const int textView1 = 2131099650;
+			
+			// aapt resource value: 0x7f060004
+			public const int textView2 = 2131099652;
+			
+			// aapt resource value: 0x7f060012
+			public const int txtCompanyName = 2131099666;
+			
+			// aapt resource value: 0x7f060011
+			public const int txtName = 2131099665;
+			
+			// aapt resource value: 0x7f060005
+			public const int txtPassword = 2131099653;
+			
+			// aapt resource value: 0x7f060003
+			public const int txtUserName = 2131099651;
 			
 			static Id()
 			{
@@ -174,25 +180,28 @@ namespace Busidex.Presentation.Android
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Login = 2130903040;
+			public const int Card = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int Login = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int MyBusidex = 2130903042;
+			public const int Main = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int MyOrganizations = 2130903043;
+			public const int MyBusidex = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int Search = 2130903044;
+			public const int MyOrganizations = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int StartUp = 2130903045;
+			public const int Search = 2130903045;
 			
 			// aapt resource value: 0x7f030006
-			public const int UserCardListItem = 2130903046;
+			public const int StartUp = 2130903046;
+			
+			// aapt resource value: 0x7f030007
+			public const int UserCardListItem = 2130903047;
 			
 			static Layout()
 			{
