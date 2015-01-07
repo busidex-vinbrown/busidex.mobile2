d8192b8b08db0ce0bd333c6daa5135b3de1bd28d
Vinny Brown <busidex.vinbrown@gmail.com>
2015-01-06 20:37:42 -0500

    Android - Organization detail layout

--- Busidex.Presentation.Android/Activities/OrganizationDetailActivity.cs
+++ Busidex.Presentation.Android/Activities/OrganizationDetailActivity.cs
@@ -10,19 +10,32 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Busidex.Mobile.Models;
 
 namespace Busidex.Presentation.Android
 {
 	[Activity (Label = "OrganizationDetailActivity")]			
 	public class OrganizationDetailActivity : Activity
 	{
+		Organization organization;
+
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			SetContentView (Resource.Layout.Organization);
 
 			base.OnCreate (savedInstanceState);
 
-			// Create your application here
+			var data = Intent.GetStringExtra ("Organization");
+			organization = Newtonsoft.Json.JsonConvert.DeserializeObject<Organization> (data);
+
+		}
+
+		public override void OnAttachedToWindow ()
+		{
+			base.OnAttachedToWindow ();
+			if(organization != null){
+				Window.SetTitle(organization.Name);
+			}
 		}
 	}
 }
