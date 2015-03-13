a8e899a375b5796d71e9da20f99bd901c6b8d7ae
Vinny Brown <busidex.vinbrown@gmail.com>
2015-03-13 17:28:31 -0400

    update for login error message

--- Busidex.Presentation.Android/Activities/LoginActivity.cs
+++ Busidex.Presentation.Android/Activities/LoginActivity.cs
@@ -10,32 +10,52 @@ namespace Busidex.Presentation.Android
 	[Activity (Label = "Sign In")]			
 	public class LoginActivity : BaseActivity
 	{
+		TextView txtLoginFailed { get; set; }
+		TextView txtUserName { get; set; }
+		TextView txtPassword { get; set; }
+
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
+			SetContentView (Resource.Layout.Login);
+
 			base.OnCreate (savedInstanceState);
 
-			SetContentView (Resource.Layout.Login);
+			txtLoginFailed = FindViewById<TextView> (Resource.Id.txtLoginFailed);
+			txtUserName = FindViewById<TextView> (Resource.Id.txtUserName);
+			txtPassword = FindViewById<TextView> (Resource.Id.txtPassword);
+
+			txtLoginFailed.Visibility = global::Android.Views.ViewStates.Gone;
+
+
 
 			var button = FindViewById<Button> (Resource.Id.btnLogin);
 			button.Click += delegate {
 				DoLogin();
 			};
+
+			txtUserName.TextChanged += (sender, e) => HideErrorMessage();
+			txtPassword.TextChanged += (sender, e) => HideErrorMessage();
+		}
+
+		void HideErrorMessage(){
+			txtLoginFailed.Visibility = global::Android.Views.ViewStates.Gone;
 		}
 
 		void DoLogin(){
-
-			var txtUserName = FindViewById<TextView> (Resource.Id.txtUserName);
-			var txtPassword = FindViewById<TextView> (Resource.Id.txtPassword);
 
 			var userName = txtUserName.Text;
 			var password = txtPassword.Text;
 
 			var response = Busidex.Mobile.LoginController.DoLogin (userName, password);
-			var loginResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<LoginResponse> (response);
-			var userId = loginResponse != null ? loginResponse.UserId : 0;
-			SetAuthCookie (userId);
+			if (!response.Contains ("404")) {
+				var loginResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<LoginResponse> (response);
+				var userId = loginResponse != null ? loginResponse.UserId : 0;
+				SetAuthCookie (userId);
 
-			RedirectToMainIfLoggedIn ();
+				RedirectToMainIfLoggedIn ();
+			}else{
+				txtLoginFailed.Visibility = global::Android.Views.ViewStates.Visible;
+			}
 		}
 
 
