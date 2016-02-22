82986f5f03967c45e141ec645750fdd4ec161a08
Vinny Brown <busidex.vinbrown@gmail.com>
2016-02-21 21:45:13 -0500

    iOS: show loading spinner if quickshare card isn't loaded yet. support horizontal and vertical cards.

--- Busidex.Presentation.iOS/Controllers/QuickShareController.cs
+++ Busidex.Presentation.iOS/Controllers/QuickShareController.cs
@@ -5,6 +5,7 @@ using Busidex.Mobile.Models;
 using System.IO;
 using Busidex.Mobile;
 using Foundation;
+using System.Drawing;
 
 namespace Busidex.Presentation.iOS
 {
@@ -46,8 +47,37 @@ namespace Busidex.Presentation.iOS
 				var cardResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<CardDetailResponse> (result);
 				var card = cardResponse.Model;
 				var fileName = Path.Combine (documentsPath, Resources.THUMBNAIL_FILE_NAME_PREFIX + card.FrontFileName);
+
+				var leftMargin = card.FrontOrientation == "H" ? 11f : 72f;
+				const float cardTop = 66f;
+				var cardWidth = card.FrontOrientation == "H" ? 300f : 180f;
+				var cardHeight = card.FrontOrientation == "H" ? 180f : 300f;
+				const float bottomMargin = 25f;
+
+
+				const float messageLeft = 20f;
+				var messageTop = cardTop + cardHeight;
+				const float messageWidth = 280f;
+				const float messageHeight = 90f;
+				lblMessage.Frame = new RectangleF (messageLeft, messageTop, messageWidth, messageHeight);
+
+				messageTop += messageHeight;
+
+				lblPersonalMessage.Frame = new RectangleF (messageLeft, messageTop, messageWidth, messageHeight);
+
+				imgSharedCard.Frame = new RectangleF (leftMargin, cardTop, cardWidth, cardHeight);
+				
 				if (File.Exists (fileName)) {
 					imgSharedCard.Image = UIImage.FromFile (fileName);
+				}else{
+					ShowOverlay ();
+
+					Utils.DownloadImage (Resources.CARD_PATH + card.FrontFileName, documentsPath, Resources.THUMBNAIL_FILE_NAME_PREFIX + card.FrontFileName).ContinueWith (t => {
+						InvokeOnMainThread (() => {
+							imgSharedCard.Image = UIImage.FromFile (fileName);
+							Overlay.Hide();
+						});
+					});
 				}
 			}
 
