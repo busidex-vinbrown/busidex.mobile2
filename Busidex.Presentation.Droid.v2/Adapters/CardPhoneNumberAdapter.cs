19a18d43c9f93809fe456b32b49997c89f585990
Vinny Brown <busidex.vinbrown@gmail.com>
2016-07-06 19:30:11 -0400

    Android: edit contact info UI done

---
+++ Busidex.Presentation.Droid.v2/Adapters/CardPhoneNumberAdapter.cs
@@ -0,0 +1,61 @@
+﻿using Android.Widget;
+using Android.Views;
+using Busidex.Mobile.Models;
+using System.Collections.Generic;
+using Android.App;
+
+namespace Busidex.Presentation.Droid.v2
+{
+	public delegate void EditPhoneNumberHandler (PhoneNumber number);
+	public delegate void DeletePhoneNumberHandler (PhoneNumber number);
+
+	public class CardPhoneNumberAdapter : ArrayAdapter<PhoneNumber>
+	{
+		public event EditPhoneNumberHandler EditPhoneNumber;
+		public event DeletePhoneNumberHandler DeletePhoneNumber;
+
+		List<PhoneNumber> PhoneNumbers { get; set; }
+
+		readonly Activity context;
+
+		public CardPhoneNumberAdapter (Activity ctx, int id, List<PhoneNumber> phoneNumbers) : base (ctx, id, phoneNumbers)
+		{
+			context = ctx;
+			PhoneNumbers = phoneNumbers;
+		}
+
+		public override int Count {
+			get {
+				return PhoneNumbers == null ? 0 : PhoneNumbers.Count;
+			}
+		}
+
+		public override View GetView (int position, View convertView, ViewGroup parent)
+		{
+			var view = convertView ?? context.LayoutInflater.Inflate (Resource.Layout.CardPhoneNumber, null);
+			var number = PhoneNumbers [position];
+
+			var lblPhoneNumberType = view.FindViewById<TextView> (Resource.Id.lblPhoneNumberType);
+			var lblPhoneNumber = view.FindViewById<TextView> (Resource.Id.lblPhoneNumber);
+			var btnEditPhoneNumber = view.FindViewById<ImageButton> (Resource.Id.btnEditPhoneNumber);
+			var btnDeletePhoneNumber = view.FindViewById<ImageButton> (Resource.Id.btnDeletePhoneNumber);
+
+			lblPhoneNumberType.Text = System.Enum.GetName (typeof (PhoneNumberTypes), number.PhoneNumberType.PhoneNumberTypeId) + ":";
+			lblPhoneNumber.Text = number.Number;
+
+			btnEditPhoneNumber.Click += delegate {
+				if (EditPhoneNumber != null) {
+					EditPhoneNumber (number);
+				}
+			};
+
+			btnDeletePhoneNumber.Click += delegate {
+				if (DeletePhoneNumber != null) {
+					DeletePhoneNumber (number);
+				}
+			};
+			return view;
+		}
+	}
+}
+
---
+++ Busidex.Presentation.Droid.v2/Adapters/PhoneNumberTypeAdapter.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using Busidex.Mobile.Models;
+
+namespace Busidex.Presentation.Droid.v2
+{
+	public class PhoneNumberTypeAdapter : ArrayAdapter<PhoneNumberType>
+	{
+		public List<PhoneNumberType> PhoneNumberTypes { get; set; }
+		private readonly Activity _context;
+		private readonly IList<View> _views = new List<View> ();
+
+		public PhoneNumberTypeAdapter (Activity ctx, int id, List<PhoneNumberType> phoneNumberTypes) : base (ctx, id, phoneNumberTypes)
+		{
+			PhoneNumberTypes = new List<PhoneNumberType> ();
+			PhoneNumberTypes.AddRange (phoneNumberTypes);
+			_context = ctx;
+		}
+
+		public PhoneNumberType GetItemAtPosition (int position)
+		{
+			return PhoneNumberTypes != null && position < PhoneNumberTypes.Count ? PhoneNumberTypes [position] : null;
+		}
+
+		public override long GetItemId (int position)
+		{
+			return position;
+		}
+
+		public override int Count {
+			get {
+				return PhoneNumberTypes == null ? 0 : PhoneNumberTypes.Count;
+			}
+		}
+
+		public override View GetDropDownView (int position, View convertView, ViewGroup parent)
+		{
+			return GetCustomView (position, convertView, parent, true);
+		}
+
+		public override View GetView (int position, View convertView, ViewGroup parent)
+		{
+			return GetCustomView (position, convertView, parent, false);
+		}
+
+		private View GetCustomView (int position, View convertView, ViewGroup parent, bool dropdown)
+		{
+			var item = PhoneNumberTypes [position];
+
+			var inflater = LayoutInflater.From (_context);
+			var view = convertView ?? inflater.Inflate (Resource.Layout.PhoneTypeSpinnerItem, parent, false);
+
+			var text = view.FindViewById<TextView> (Resource.Id.txtSpinnerText);
+
+			if (text != null)
+				text.Text = item.Name;
+
+			if (!_views.Contains (view))
+				_views.Add (view);
+
+			return view;
+		}
+
+		private void ClearViews ()
+		{
+			foreach (var view in _views) {
+				view.Dispose ();
+			}
+			_views.Clear ();
+		}
+
+		protected override void Dispose (bool disposing)
+		{
+			ClearViews ();
+			base.Dispose (disposing);
+		}
+	}
+}
+
