e8ef829845a23a8e649c9abc02444d34eb2683bb
Vinny Brown <busidex.vinbrown@gmail.com>
2016-07-10 21:16:08 -0400

    Android: updated test packages. Started Image edit UI

--- Busidex.Presentation.Droid.v2/Fragments/CardImageEditFragment.cs
+++ Busidex.Presentation.Droid.v2/Fragments/CardImageEditFragment.cs
@@ -1,14 +1,4 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Util;
+﻿using Android.OS;
 using Android.Views;
 using Android.Widget;
 
@@ -16,6 +6,23 @@ namespace Busidex.Presentation.Droid.v2
 {
 	public class CardImageEditFragment : BaseCardEditFragment
 	{
+		ImageButton btnBack;
+		Button btnCardFront;
+		Button btnCardBack;
+
+		enum CardEditMode
+		{
+			Front,
+			Back
+		}
+
+		public override void OnDetach ()
+		{
+			btnBack = null;
+			btnCardFront = null;
+			btnCardFront = null;
+		}
+
 		public override void OnResume ()
 		{
 			base.OnResume ();
@@ -27,14 +34,50 @@ namespace Busidex.Presentation.Droid.v2
 
 		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
-			var view = inflater.Inflate (Resource.Layout.CardImageEdit, container, false);
+			view = inflater.Inflate (Resource.Layout.CardImageEdit, container, false);
 
-			var btnBack = view.FindViewById<ImageButton> (Resource.Id.btnBack);
+			base.OnCreateView (inflater, container, savedInstanceState);
+
+			updateCover = view.FindViewById<RelativeLayout> (Resource.Id.updateCover);
+			updateCover.Visibility = ViewStates.Gone;
+
+			btnCardFront = view.FindViewById<Button> (Resource.Id.btnCardFront);
+			btnCardBack = view.FindViewById<Button> (Resource.Id.btnCardBack);
+
+			btnCardFront.Click += delegate {
+				toggle (CardEditMode.Front);
+			};
+
+			btnCardBack.Click += delegate {
+				toggle (CardEditMode.Back);
+			};
+
+			btnBack = view.FindViewById<ImageButton> (Resource.Id.btnBack);
 			btnBack.Click += delegate {
 				((MainActivity)Activity).LoadFragment (new CardMenuFragment ());
 			};
 
 			return view;
+		}
+
+		void toggle (CardEditMode mode)
+		{
+			switch (mode) {
+			case CardEditMode.Front: {
+					btnCardFront.SetBackgroundResource (Resource.Color.buttonFontColor);
+					btnCardFront.SetTextColor (Resources.GetColor (Resource.Color.buttonWhite));
+					btnCardBack.SetBackgroundResource (Resource.Color.buttonWhite);
+					btnCardBack.SetTextColor (Resources.GetColor (Resource.Color.buttonFontColor));
+					break;
+				}
+			case CardEditMode.Back: {
+					btnCardFront.SetBackgroundResource (Resource.Color.buttonWhite);
+					btnCardFront.SetTextColor (Resources.GetColor (Resource.Color.buttonFontColor));
+					btnCardBack.SetBackgroundResource (Resource.Color.buttonFontColor);
+					btnCardBack.SetTextColor (Resources.GetColor (Resource.Color.buttonWhite));
+					break;
+				}
+			}
 		}
 	}
 }
