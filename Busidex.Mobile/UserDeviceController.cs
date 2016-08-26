86e9b6c8894e9f02489b2b9d7d44d82504a52716
Vinny Brown <busidex.vinbrown@gmail.com>
2016-08-25 20:36:27 -0400

    save user device info

---
+++ Busidex.Mobile/UserDeviceController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+
+namespace Busidex.Mobile
+{
+	public class UserDeviceController : BaseController
+	{
+		public static async Task<bool> UpdateDeviceDetails (DeviceType deviceType, int version, string token)
+		{
+
+			var data = new UserDevice {
+				UserId = 0,
+				DeviceTypeId = (int)deviceType,
+				Version = version
+			};
+
+			const string URL = Resources.BASE_API_URL + "UserDevice/UpdateUserDevice";
+
+			await MakeRequestAsync (URL, "PUT", token, data, new ModernHttpClient.NativeMessageHandler ());
+
+			return true;
+		}
+
+		public static async Task<UserDevice> GetUserDevice (DeviceType deviceType, string token)
+		{
+			string URL = Resources.BASE_API_URL + "UserDevice/GetDeviceDetails?deviceType=" + deviceType;
+
+			var result = await MakeRequestAsync (URL, "GET", token, null, new ModernHttpClient.NativeMessageHandler ());
+			var userDevice = Newtonsoft.Json.JsonConvert.DeserializeObject<UserDevice> (result);
+			return userDevice;
+		}
+
+		public static async Task<AppVersionInfo> GetCurrentAppInfo (string token)
+		{
+			string URL = Resources.BASE_API_URL + "UserDevice/GetCurrentAppInfo";
+
+			var result = await MakeRequestAsync (URL, "GET", token, null, new ModernHttpClient.NativeMessageHandler ());
+			var appInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<AppVersionInfo> (result);
+			return appInfo;
+		}
+	}
+}
+
